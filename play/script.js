884a4ef03a8544d36d31fb783786de3151bae03b
HasanKanj <73579930+HasanKanj@users.noreply.github.com>
2022-12-12 21:47:54 +0300

    Add files via upload

---
+++ play/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+
+    if(document.getElementsByClassName('kanj').checked) {
+        document.getElementById('linkid').href = "https://www.geeksforgeeks.org";
+    }
+
+  if(document.getElementById('kanj').checked) {
+        document.getElementsByClassName('hasan').href = "https://www.geeksforgeeks.org";
+    }
+
+
+}
