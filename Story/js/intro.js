bfa6b6e6f2cfc30ad951ae6bf7b0e29e1311a880
hasan <hasankanj2000@gmail.com>
2022-12-13 22:13:01 +0300

    story

---
+++ Story/js/intro.js
@@ -0,0 +1,184 @@
+function myFunc() {
+  var x = document.getElementById("myAudio").autoplay;
+  document.getElementById("demo").innerHTML = x;
+}
+
+
+
+function myFunction() {
+
+
+  if (document.getElementById("1").checked == true) {
+    window.location.href = "index3.html"
+  }
+  else if (document.getElementById("2").checked == true) {
+
+    window.location.href = "index2.html"
+  }
+
+}
+
+function getSelectItemThat(id) {
+  for (var i = 1; i <= 2; i++) {
+    document.getElementById(i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+function myFunction2() {
+
+
+  if (document.getElementById("1").checked == true) {
+    window.location.href = "index3.html"
+  }
+  else if (document.getElementById("2").checked == true) {
+
+    window.location.href = "index4.html"
+  }
+
+}
+
+function getSelectItemThat(id) {
+  for (var i = 1; i <= 2; i++) {
+    document.getElementById(i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+
+function myFunction3() {
+
+
+  if (document.getElementById("1").checked == true) {
+    window.location.href = "index5.html"
+  }
+  else if (document.getElementById("2").checked == true) {
+
+    window.location.href = "index6.html"
+  }
+
+}
+
+
+function getSelectItemThat(id) {
+  for (var i = 1; i <= 2; i++) {
+    document.getElementById(i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+function myFunction4() {
+
+
+
+  window.location.href = "introduction.html"
+
+
+}
+
+
+function getSelectItemThat(id) {
+  for (var i = 1; i <= 2; i++) {
+    document.getElementById(i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+
+function myFunction5() {
+
+  window.location.href = "introduction.html"
+
+}
+
+
+function myFunction6() {
+
+
+  if (document.getElementById("1").checked == true) {
+    window.location.href = "index7.html"
+  }
+  else if (document.getElementById("2").checked == true) {
+
+    window.location.href = "index8.html"
+  }
+
+}
+
+function getSelectItemThat(id) {
+  for (var i = 1; i <= 2; i++) {
+    document.getElementById(i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+
+
+function myFunction7() {
+
+
+  if (document.getElementById("1").checked == true) {
+    window.location.href = "index9.html"
+  }
+  else if (document.getElementById("2").checked == true) {
+
+    window.location.href = "index10.html"
+  }
+
+}
+
+function getSelectItemThat(id) {
+  for (var i = 1; i <= 2; i++) {
+    document.getElementById(i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+
+function myFunction8() {
+
+
+
+  window.location.href = "introduction.html"
+
+}
+
+function myFunction9() {
+
+
+
+  window.location.href = "introduction.html"
+
+
+}
+
+
+
+function myFunction10() {
+
+
+  if (document.getElementById("1").checked == true) {
+    window.location.href = "index11.html"
+  }
+  else if (document.getElementById("2").checked == true) {
+
+    window.location.href = "index12.html"
+  }
+
+}
+
+function getSelectItemThat(id) {
+  for (var i = 1; i <= 2; i++) {
+    document.getElementById(i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+function myFunction11() {
+
+
+
+  window.location.href = "introduction.html"
+
+
+}
